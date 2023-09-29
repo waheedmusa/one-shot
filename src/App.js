b96d0d9b363b06ba65d80fff88d45bd7f613ea14
waheedmusa <abdulwaheedmusa0@gmail.com>
2023-09-29 07:08:12 +0300

    changes made

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route exact path="/results" element={<Results />} />
